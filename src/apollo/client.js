41899e5b454166a5f68dfdf64e9690008e7a8b28
snowman-1030 <pandahive0914@gmail.com>
2025-12-03 17:11:30 -0800

    update backend url

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: process.env.REACT_APP_GRAPHQL_URL || 'https://orally-intercosmic-clair.ngrok-free.dev/graphql',
+  uri: process.env.REACT_APP_GRAPHQL_URL || 'https://test-backend-eta-drab.vercel.app/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
